885e08f4194b58ad61935749265b9c3ad3f2d179
shibang <shibang@aliyun.com>
2019-01-05 23:56:34 +0800

    add bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,32 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 设置背景色
     bg_color = (230, 230, 230)
     #开始游戏的主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
 
 run_game()
--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship():
     def __init__(self, ai_settings, screen):
         """初始化飞船并设置其初始位置"""
